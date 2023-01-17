ae6329396545a9f2d4eab756c6cf1fd7d2854382
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-01-17 14:54:35 +0000

    chore(release): v5.0.0

--- null/go.mod
+++ null/go.mod
@@ -1,9 +1,9 @@
-module github.com/cdktf/cdktf-provider-null-go/null/v4
+module github.com/cdktf/cdktf-provider-null-go/null/v5
 
 go 1.18
 
 require (
-	github.com/aws/jsii-runtime-go v1.71.0
-	github.com/hashicorp/terraform-cdk-go/cdktf v0.14.0
-	github.com/aws/constructs-go/constructs/v10 v10.1.161
+	github.com/aws/jsii-runtime-go v1.73.0
+	github.com/hashicorp/terraform-cdk-go/cdktf v0.15.0
+	github.com/aws/constructs-go/constructs/v10 v10.1.215
 )
--- null/resource/resource_Resource.go
+++ null/resource/resource_Resource.go
@@ -2,10 +2,10 @@ package resource
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v5/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-null-go/null/v4/resource/internal"
+	"github.com/cdktf/cdktf-provider-null-go/null/v5/resource/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
