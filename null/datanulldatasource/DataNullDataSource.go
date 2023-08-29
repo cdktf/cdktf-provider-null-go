6714281a0d1f350b141afbca38c4308163d2778f
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-08-29 14:58:43 +0000

    chore(release): v8.0.0

--- null/datanulldatasource/DataNullDataSource.go
+++ null/datanulldatasource/DataNullDataSource.go
@@ -1,11 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package datanulldatasource
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v7/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v8/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-null-go/null/v7/datanulldatasource/internal"
+	"github.com/cdktf/cdktf-provider-null-go/null/v8/datanulldatasource/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
