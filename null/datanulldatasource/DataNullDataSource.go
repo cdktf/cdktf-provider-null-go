894ab236ac075190155cb3bf5a4890f0e55265db
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-06-04 18:59:15 +0000

    chore(release): v11.0.0

--- null/datanulldatasource/DataNullDataSource.go
+++ null/datanulldatasource/DataNullDataSource.go
@@ -5,10 +5,10 @@ package datanulldatasource
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v10/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v11/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-null-go/null/v10/datanulldatasource/internal"
+	"github.com/cdktf/cdktf-provider-null-go/null/v11/datanulldatasource/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- null/provider/NullProvider.go
+++ null/provider/NullProvider.go
@@ -5,10 +5,10 @@ package provider
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v10/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-null-go/null/v11/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-null-go/null/v10/provider/internal"
+	"github.com/cdktf/cdktf-provider-null-go/null/v11/provider/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
